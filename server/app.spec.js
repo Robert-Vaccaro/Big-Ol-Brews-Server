2ffd4e49d2af6479d96896c8ba3b7b78e862251e
Robert-Vaccaro <48065298+Robert-Vaccaro@users.noreply.github.com>
2019-10-27 14:20:41 -0700

    Update app.spec.js

--- server/app.spec.js
+++ server/app.spec.js
@@ -2,7 +2,7 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing "Howdy, boilerplate"', () => {
     return supertest(app)
       .get('/')
       .expect(200, 'Howdy, boilerplate')
